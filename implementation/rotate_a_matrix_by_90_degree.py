fa91107d3c2dea96929533905166fdfc7a4558f1
Gimsongsam <gimsong83@gmail.com>
2021-11-02 17:58:43 +0900

    rotate a martix by 90 degree

---
+++ implementation/rotate_a_matrix_by_90_degree.py
@@ -0,0 +1,10 @@
+# 리스트를 90도 회전시키는 함수
+
+def rotate_a_matrix_by_90_degree(a):
+    n = len(a) # 행 길이 계산
+    m = len(a) # 열 길이 계산
+    result = [[0] * n for _ in range(m)] # 결과 리스트
+    for i in range(n):
+        for j in range(m):
+            result[j][n - i - 1] = a[i][j]
+    return result
